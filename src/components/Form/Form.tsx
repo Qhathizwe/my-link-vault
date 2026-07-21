76ce54f06f8b8d654468d5649f3d06b9ad148245
qhathizwe <sphoswa2@gmail.com>
2026-07-21 15:20:06 +0200

    useState hook

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -6,26 +6,27 @@ export const Form = () => {
       <h1>Mi-Link<span>-Vault</span></h1>
 
       <div className={styles.Vault_Container}>
+
         <div className={styles.Vault_inputs}>
 
             <div className={styles.title_input}>
             <label>Title :</label>
-            <input type='text' alt='title' placeholder='what is the link title'  style={{width: '300px', height: '20px'}}/>
+            <input type='text' alt='title' placeholder='what is the link title' id={'title'}/>
             </div>
             
             <div className={styles.link_input}>
             <label>Link :</label>
-            <input type='text' alt='Link' placeholder='Paste your link here' />
+            <input type='text' alt='Link' placeholder='Paste your link here' id={'link'}/>
             </div>
 
             <div className={styles.Description_input}>
             <label >Description :</label>
-            <textarea placeholder='links Description here ' className={styles.description_textArea}></textarea>
+            <textarea placeholder='links Description here ' className={styles.description_textArea} id={'description'}></textarea>
             </div>
 
             <div className={styles.Tags_input}>
             <label>Tags :</label>
-            <input type='text' alt='Tags' placeholder='Type of Tag (optional)' />
+            <input type='text' alt='Tags' placeholder='Type of Tag (optional)' id={'tags'}/>
             </div>
 
             <button className={styles.btn_add}>Add Link</button>
---
+++ src/components/Form/FormHooks.tsx
@@ -0,0 +1,13 @@
+import React, {useState} from 'react'
+import Form from '../Form/Form'
+
+export const FormHooks = () => {
+ 
+    const [title, setTitle] = useState('');
+    const [link, setLink] = useState('');
+    const [description, setDescription] = useState('');
+    const [tags, setTags] =useState ('');
+    
+
+
+}
